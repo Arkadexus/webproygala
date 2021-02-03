d7cfbb7a75ec130d162609f05747c7b8319d65a6
Arkadexus <eduardse2001@gmail.com>
2021-02-04 00:39:36 +0100

    Slider casi terminado

--- index.php
+++ index.php
@@ -14,16 +14,24 @@
     ?>
 
 <div class="contenido">
-    <img id="imagen-principal" src="img/hotel-index.jpg" alt="slider primera imagen">
+    <div>
+        <img class="mySlides fade" src="img/hotel-index.jpg">
+        <img class="mySlides fade" src="img/hotel-index.jpg">
+        <img class="mySlides fade" src="img/hotel-index.jpg">
+        <div class="botones">
+            <span class="punto" onclick="currentSlide(1)"></span>
+            <span class="punto" onclick="currentSlide(2)"></span>
+            <span class="punto" onclick="currentSlide(3)"></span>
+        </div>
+    </div>
     <h3>Esto es un test</h3>
     <p>Si esto funciona, significa que la integración con github ha funcionadooooo</p>
     <?php
-        for($i = 0; $i < 15; $i++){
+        for($i = 0; $i < 50; $i++){
             echo("HOLA <br/>");
         }
     ?>
 </div>
-
-    <!--<script src="scripts/alert.js"></script>-->
+<script src="scripts/slider.js"></script>
 </body>
 </html>

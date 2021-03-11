0dafc8643634b535c267549e38b6b01c354644db
Arkadexus <eduardse2001@gmail.com>
2021-03-11 19:20:26 +0100

    Slider cambiado

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@splidejs/splide@latest/dist/css/splide.min.css">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <title>Gala d'Or</title>
 </head>
@@ -14,24 +15,24 @@
     ?>
 
 <div class="contenido">
-    <div>
-        <img class="mySlides fade" src="img/hotel-index.jpg">
-        <img class="mySlides fade" src="img/hotel-index.jpg">
-        <img class="mySlides fade" src="img/hotel-index.jpg">
-        <div class="botones">
-            <span class="punto" onclick="currentSlide(1)"></span>
-            <span class="punto" onclick="currentSlide(2)"></span>
-            <span class="punto" onclick="currentSlide(3)"></span>
-        </div>
-    </div>
+<div class="splide">
+	<div class="splide__track">
+		<ul class="splide__list">
+			<li class="splide__slide"><img src="img/hotel-index.jpg" alt="slide hotel 1"></li>
+			<li class="splide__slide"><img src="img/hotel-index2.jpg" alt="slide hotel 2"></li>
+			<li class="splide__slide"><img src="img/hotel-index3.jpg" alt="slide hotel 2"></li>
+		</ul>
+	</div>
+</div>
     <h3>Esto es un test</h3>
     <p>Si esto funciona, significa que la integración con github ha funcionadooooo</p>
     <?php
         for($i = 0; $i < 50; $i++){
-            echo("HOLA <br/>");
+            echo("<p>HOLA</p>");
         }
     ?>
 </div>
-<script src="scripts/slider.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@latest/dist/js/splide.min.js"></script>
+<script src="scripts/cargarSlider.js"></script>
 </body>
 </html>
---
+++ scripts/cargarSlider.js
@@ -0,0 +1,9 @@
+new Splide( '.splide', {
+    type   : 'loop',
+    'cover'      : true,
+	padding: {
+		right: '5rem',
+		left : '5rem',
+    },
+    autoplay: true,
+} ).mount();

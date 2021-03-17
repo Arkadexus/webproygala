b75f92fd37d3a39afecc732aa8da602d62c4857a
Arkadexus <eduardse2001@gmail.com>
2021-03-17 20:55:35 +0100

    Slider prototipo definitivo

--- index.php
+++ index.php
@@ -18,11 +18,18 @@
     <div class="splide">
         <div class="splide__track">
             <ul class="splide__list">
-                <li class="splide__slide"><h2>Lorem ipsum dolor sit amet</h2><span>Lorem ipsum dolor sit amet</span><img src="img/imageonline-co-darkenimage.png" alt="slide hotel 1"></li>
-                <li class="splide__slide"><h2>Lorem ipsum dolor sit amet</h2><span>Lorem ipsum dolor sit amet</span><img src="img/imageonline-co-darkenimage (2).png" alt="slide hotel 2"></li>
-                <li class="splide__slide"><h2>Lorem ipsum dolor sit amet</h2><span>Lorem ipsum dolor sit amet</span><img src="img/imageonline-co-darkenimage (3).png" alt="slide hotel 2"></li>
+                <li class="splide__slide"><h2>Lorem <i>ipsum</i> dolor sit amet</h2><span>Lorem ipsum dolor sit amet</span><img src="img/slider1.png" alt="slide hotel 1"></li>
+                <li class="splide__slide"><h2>Lorem ipsum dolor sit amet</h2><span>Lorem ipsum dolor sit amet</span><img src="img/slider2.png" alt="slide hotel 2"></li>
+                <li class="splide__slide"><h2>Lorem ipsum dolor sit amet</h2><span>Lorem ipsum dolor sit amet</span><img src="img/slider3.png" alt="slide hotel 3"></li>
             </ul>
         </div>
+    <!--
+    https://pixabay.com/es/photos/paisaje-hotel-complejo-paisajes-2016308/
+    https://pixabay.com/es/photos/hotel-berl%C3%ADn-kurf%C3%BCrstendamm-ciudad-951599/
+    https://pixabay.com/es/photos/complejo-hotel-playa-%C3%A1rbol-956829/
+    https://pixabay.com/es/photos/sill%C3%B3n-sof%C3%A1-decoraci%C3%B3n-almohada-2608761/
+    https://pixabay.com/es/photos/caf%C3%A9-hotel-cama-holiday-servicio-4507707/
+    -->
     </div>
     <h3>Esto es un test</h3>
     <p>Si esto funciona, significa que la integración con github ha funcionadooooo</p>
--- scripts/sliderHome.js
+++ scripts/sliderHome.js
@@ -1,7 +1,7 @@
 new Splide( '.splide', {
     type: 'loop',
     width : '100vw',
-	height: '70vh',
+	height: '100vh',
     cover: true,
     autoplay: true,
     interval: 5000,
